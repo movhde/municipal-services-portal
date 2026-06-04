948c87679d14a0e5c6beff7c44c0b50a03838662
movhde <movahedesalehivaziri@gmail.com>
2026-06-04 15:55:01 +0330

    feat: add header component

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,14 @@
+import LandingView from '@/views/LandingView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      component: LandingView,
+    },
+  ],
 })
 
 export default router
---
+++ src/views/LandingView.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import AppHeader from '@/components/layout/AppHeader.vue'
+</script>
+
+<template>
+  <AppHeader />
+</template>
