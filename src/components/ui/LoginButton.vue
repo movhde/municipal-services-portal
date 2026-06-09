84be33379c94bfbfdbc68c257b52616e06b7257c
movhde <movahedesalehivaziri@gmail.com>
2026-06-10 01:56:46 +0330

    feat(store): persist auth state to localStorage

--- src/components/ui/LoginButton.vue
+++ src/components/ui/LoginButton.vue
@@ -1,7 +1,21 @@
+<script setup lang="ts">
+import { useAuthStore } from '@/stores/auth'
+
+const authStore = useAuthStore()
+</script>
+
 <template>
-  <div class="w-fit px-3 py-1 flex items-center gap-2 bg-[#d7e7ec] rounded-2xl">
+  <div class="w-fit px-3 py-2 flex items-center gap-2 bg-[#d7e7ec] rounded-2xl">
     <i class="pi pi-user text-[#2a7e97]"> </i>
-    <div class="flex items-center gap-1 text-gray-500">
+    <div v-if="authStore.isLoggedIn">
+      <RouterLink
+        to="/user-profile"
+        class="text-xs md:text-sm text-gray-500 hover:text-[#2a7e97] transition-colors duration-300 whitespace-nowrap"
+        >{{ authStore.user?.firstName + ' ' + authStore.user?.lastName }}</RouterLink
+      >
+    </div>
+
+    <div v-else class="flex items-center gap-1 text-gray-500">
       <RouterLink
         to="/login"
         class="text-xs md:text-sm hover:text-[#2a7e97] transition-colors duration-300"
--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -1,21 +1,33 @@
 import type { User } from '@/types/auth'
 import { defineStore } from 'pinia'
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 
 export const useAuthStore = defineStore('auth', () => {
   const user = ref<User | null>(null)
-  const isLoggedIn = ref(false)
+
+  const isLoggedIn = computed(() => user.value !== null)
 
   const login = (userData: User) => {
     user.value = userData
-    console.log('User logged in:', user.value)
-    isLoggedIn.value = true
+
+    localStorage.setItem('user', JSON.stringify(userData))
   }
 
   const logout = () => {
     user.value = null
-    isLoggedIn.value = false
+
+    localStorage.removeItem('user')
   }
 
-  return { user, isLoggedIn, login, logout }
+  const initializeAuth = () => {
+    const storedUser = localStorage.getItem('user')
+
+    if (storedUser) {
+      user.value = JSON.parse(storedUser)
+    }
+  }
+
+  initializeAuth()
+
+  return { user, isLoggedIn, login, logout, initializeAuth }
 })
